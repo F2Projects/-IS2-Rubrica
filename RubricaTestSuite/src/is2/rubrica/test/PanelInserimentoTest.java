112f06d5377c504db28264257720b3cc70e21068
Pasquale Boemio <boemianrapsodi@gmail.com>
2012-07-23 12:07:59 +0200

    more tests... next level testss

--- RubricaTestSuite/src/is2/rubrica/test/PanelInserimentoTest.java
+++ RubricaTestSuite/src/is2/rubrica/test/PanelInserimentoTest.java
@@ -1,5 +1,7 @@
 package is2.rubrica.test;
 
+import java.util.Random;
+
 import is2.rubrica.Rubrica;
 import org.uispec4j.UISpecTestCase;
 import org.uispec4j.Window;
@@ -7,26 +9,167 @@ import org.uispec4j.interception.MainClassAdapter;
 
 public class PanelInserimentoTest extends UISpecTestCase {
 	
+	private String thisTestRubName = "GuiTest-" + new Random().nextInt();
+	
 	protected void setUp() {
 		this.setAdapter(new MainClassAdapter(Rubrica.class, new String[0]));
 	
 	}
 	
+	/**
+	 * Testing path
+	 * PANNELLO INIZIALE -> bottone "Cerca Contatti" -> PANNELLO RICERCA -> bottone "Indietro" -> PANNELLO INIZIALE
+	 */
 	public void testGoToPannelloCerca() {
+		// parto da index
 		pannelloIndexVisible(true);
 		
+		// vado in cerca contatti
 		getMainWindow().getButton("Cerca Contatti").click();
 		
+		// ci sono davvero?
 		pannelloIndexVisible(false);
 		pannelloCercaVisible(true);
 		
+		// vado via
 		getMainWindow().getButton("Indietro").click();
 		
+		// me ne sono andato?
 		pannelloIndexVisible(true);
 		pannelloCercaVisible(false);
 	
 	}
 	
+	/**
+	 * Testing path
+	 * PANNELLO INIZIALE -> bottone "Visualizza contatti" -> PANNELLO INSERIMENTO NOME RUBRICA -> bottone "Indietro" -> PANNELLO INIZIALE
+	 */
+	public void testGoToVisualizzaContatti() {
+		// parto da index
+		pannelloIndexVisible(true);
+		
+		// vado in visualizza contatti
+		getMainWindow().getButton("Visualizza contatti").click();
+		
+		// ci sono davvero?
+		pannelloIndexVisible(false);
+		pannelloRubStpVisible(true);
+		
+		// vado via
+		getMainWindow().getButton("Indietro").click();
+		
+		// me ne sono andato?
+		pannelloIndexVisible(true);
+		pannelloRubStpVisible(false);
+	
+	}
+	
+	/**
+	 * Testing path
+	 * PANNELLO INIZIALE -> bottone "Crea Rubrica" -> PANNELLO CREAZIONE RUBRICA -> bottone "Indietro" -> PANNELLO INIZIALE
+	 */
+	public void testGoToCreazioneRubrica(){
+		// parto da index
+		pannelloIndexVisible(true);
+		
+		// vado in crea rubrica
+		getMainWindow().getButton("Crea rubrica").click();
+		
+		// ci sono davvero?
+		pannelloRubInsVisible(true);
+		pannelloIndexVisible(false);
+		
+		// vado via
+		getMainWindow().getButton("Indietro").click();
+		
+		// me ne sono andato?
+		pannelloRubInsVisible(false);
+		pannelloIndexVisible(true);
+	}
+	
+	/**
+	 * Testing if the user submit a void name into Creazione Rubrica
+	 */
+	public void testVoidNameCreazioneRubrica(){
+		// parto da index
+		pannelloIndexVisible(true);
+		
+		// vado in creazione rubrica
+		getMainWindow().getButton("Crea rubrica").click();
+		
+		// setto un campo nullo a inNameRub
+		getMainWindow().getTextBox("inNameRub").setText("");
+		getMainWindow().getButton("Invio").click();
+		
+		// non mi sono mosso da creazione rubrica
+		pannelloRubInsVisible(true);
+		pannelloInputVisible(false);
+		
+		// controllo che l'errore venga stampato
+		assertEquals("Nome File non corretto.Riprova                        ", getMainWindow().getTextBox("testoIns").getText());
+		
+		// vado via
+		getMainWindow().getButton("Indietro").click();
+		
+	}
+
+	/**
+	 * Testing if the user submit a valid name into Creazione Rubrica
+	 */
+	public void testValidNameCreazioneRubrica(){
+		// parto da index
+		pannelloIndexVisible(true);
+		
+		// vado in creazione rubrica
+		getMainWindow().getButton("Crea rubrica").click();
+		
+		// setto un campo nullo a inNameRub
+		getMainWindow().getTextBox("inNameRub").setText(this.thisTestRubName);
+		getMainWindow().getButton("Invio").click();
+		
+		// vado in aggiungi contatto
+		pannelloRubInsVisible(false);
+		pannelloInputVisible(true);
+		
+		// torno indietro
+		getMainWindow().getButton("Save & Back").click();
+		
+		// me ne sono andato?
+		pannelloInputVisible(false);
+		pannelloIndexVisible(true);
+
+	}
+
+	/**
+	 * Testing if the user submit a duplicate name into Creazione Rubrica
+	 */
+	public void testDuplicateNameCreazioneRubrica(){
+		// parto da index
+		pannelloIndexVisible(true);
+				
+		// vado in creazione rubrica
+		getMainWindow().getButton("Crea rubrica").click();
+		
+		// setto un campo nullo a inNameRub
+		getMainWindow().getTextBox("inNameRub").setText(this.thisTestRubName);
+		getMainWindow().getButton("Invio").click();
+		
+		// non mi sono mosso da creazione rubrica
+		pannelloRubInsVisible(true);
+		pannelloInputVisible(false);
+		
+		// controllo che l'errore venga stampato
+		assertEquals("File già esistente. Riprova                         ", getMainWindow().getTextBox("testoIns").getText());
+		
+		// vado via
+		getMainWindow().getButton("Indietro").click();
+	}
+	
+	/**
+	 * Method used to check if the Index panel is shown/hidden
+	 * 
+	 * @param att : true is for visible, false is for not visible
+	 */
 	private void pannelloIndexVisible(boolean att) {
 		Window window = getMainWindow();
 		
@@ -41,6 +184,11 @@ public class PanelInserimentoTest extends UISpecTestCase {
 
 	}
 	
+	/**
+	 * Method used to check if the Cerca panel is shown/hidden
+	 * 
+	 * @param att : true is for visible, false is for not visible
+	 */
 	private void pannelloCercaVisible(boolean att) {
 		Window window = getMainWindow();
 		
@@ -54,6 +202,11 @@ public class PanelInserimentoTest extends UISpecTestCase {
 	
 	}
    
+	/**
+	 * Method used to check if the Risultati panel is shown/hidden
+	 * 
+	 * @param att : true is for visible, false is for not visible
+	 */
 	private void pannelloRisultatiVisible(boolean att) {
 		Window window = getMainWindow();
 		
@@ -63,6 +216,11 @@ public class PanelInserimentoTest extends UISpecTestCase {
 	
 	}
 
+	/**
+	 * Method used to check if the Input panel is shown/hidden
+	 * 
+	 * @param att : true is for visible, false is for not visible
+	 */
 	private void pannelloInputVisible(boolean att) {
 		Window window = getMainWindow();
 		
@@ -81,6 +239,11 @@ public class PanelInserimentoTest extends UISpecTestCase {
 
 	}
 	
+	/**
+	 * Method used to check if the Output panel is shown/hidden
+	 * 
+	 * @param att : true is for visible, false is for not visible
+	 */
 	private void pannelloOutputVisible(boolean att) {
 		Window window = getMainWindow();
 		
@@ -95,6 +258,11 @@ public class PanelInserimentoTest extends UISpecTestCase {
 		
 	}
 	
+	/**
+	 * Method used to check if the RubIns panel is shown/hidden
+	 * 
+	 * @param att : true is for visible, false is for not visible
+	 */
 	private void pannelloRubInsVisible(boolean att) {
 		Window window = getMainWindow();
 		
@@ -108,7 +276,12 @@ public class PanelInserimentoTest extends UISpecTestCase {
 
 	}
 	
-	private void pannelloRubStp(boolean att) {
+	/**
+	 * Method used to check if the RubStp panel is shown/hidden
+	 * 
+	 * @param att : true is for visible, false is for not visible
+	 */
+	private void pannelloRubStpVisible(boolean att) {
 		Window window = getMainWindow();
 
 		assertFalse(window.getTextBox("testoIns").isVisible());
